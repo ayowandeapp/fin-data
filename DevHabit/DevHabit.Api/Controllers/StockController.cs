a0ca55e986e6879ab797b77de2219fb1cc4e2139
Ayowande Oluwatosin <ayooluwa71@gmail.com>
2025-05-10 03:02:30 +0100

    added stockController"

---
+++ DevHabit/DevHabit.Api/Controllers/StockController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+// using Microsoft.EntityFrameworkCore;
+using DevHabit.Api.Data;
+
+namespace DevHabit.Api.Controllers
+{
+    [ApiController]
+    [Route("api/stock")]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+        public StockController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stock = _context.Stocks.ToList();
+            return Ok(stock);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var stock = _context.Stocks.Find(id);
+            if(stock == null)
+            {
+                return NotFound();
+            }
+            return Ok(stock);
+
+        }
+        
+    }
+}
--- DevHabit/DevHabit.Api/obj/Debug/net9.0/DevHabit.Api.AssemblyInfo.cs
+++ DevHabit/DevHabit.Api/obj/Debug/net9.0/DevHabit.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevHabit.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cae885253747f8542baee48e34877a93a27e7f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevHabit.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevHabit.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
