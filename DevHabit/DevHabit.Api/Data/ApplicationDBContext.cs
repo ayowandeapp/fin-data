1cae885253747f8542baee48e34877a93a27e7f4
Ayowande Oluwatosin <ayooluwa71@gmail.com>
2025-05-10 01:59:28 +0100

    Models and Migrations

---
+++ DevHabit/DevHabit.Api/Data/ApplicationDBContext.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DevHabit.Api.Models; 
+
+namespace DevHabit.Api.Data
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions)
+        : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        
+    }
+}
